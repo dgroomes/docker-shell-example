341a2bab109e61794105d3d3ef06cff115275a8f
davidgroomes <dwgroomes@gmail.com>
2019-10-25 21:34:42 -0500

    Detect shell's login/non-login and interactive/non-interactive

--- debug.sh
+++ debug.sh
@@ -6,10 +6,18 @@ if [ -z "$DESCRIPTION" ]; then
   exit 1
 fi
 
+case $- in
+  *i*) IS_INTERACTIVE="interactive";;
+  *) IS_INTERACTIVE="non-interactive";;
+esac
+
+IS_LOGIN=$(shopt -q login_shell && echo 'login' || echo 'non-login')
+
 cat << EOF >> /debug_output
 **** '$DESCRIPTION' ***
 
-SHELL: $SHELL
-
+SHELL: ${SHELL:-unknown}
+$IS_INTERACTIVE
+$IS_LOGIN
 *********************
 EOF
